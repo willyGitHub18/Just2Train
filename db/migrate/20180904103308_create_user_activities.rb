31037f5209e17c30420baec0e1a83e6e3f0b9ac9
willy <wilg1569@gmail.com>
2018-09-04 12:39:56 +0200

    Creations des tables/models hors user

---
+++ db/migrate/20180904103308_create_user_activities.rb
@@ -0,0 +1,10 @@
+class CreateUserActivities < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user_activities do |t|
+      t.integer :user_id
+      t.integer :activity_id
+
+      t.timestamps
+    end
+  end
+end
