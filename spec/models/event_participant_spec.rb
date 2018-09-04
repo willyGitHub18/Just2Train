767a4be321cad7d50dd8535b1655487a8066c5ef
willy <wilg1569@gmail.com>
2018-09-04 17:51:22 +0200

    préparation tests tables jointure

--- spec/models/event_participant_spec.rb
+++ spec/models/event_participant_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe EventParticipant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Use of subject method so the tests will apply to the subject
+  subject{ described_class.new(activity_name: "some_activity") }
+
+  describe "Validation" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without a type" do
+      subject.activity_name = nil
+      expect(subject).to_not be_valid
+    end
+  end
 end
