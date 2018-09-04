02f43ea2f9d2ebdd9833d88daaf06a7bac0ee639
willy <wilg1569@gmail.com>
2018-09-04 18:13:21 +0200

    Test Association pour tables de jointure

--- spec/models/user_activity_spec.rb
+++ spec/models/user_activity_spec.rb
@@ -14,4 +14,17 @@ RSpec.describe UserActivity, type: :model do
       expect(subject).to_not be_valid
     end
   end
+
+  # Use of reflect_on_association method which will return information about the given association
+  describe "Associations" do
+    it "belongs to an activity" do
+      assc = described_class.reflect_on_association(:activity)
+      expect(assc.macro).to eq :belongs_to
+    end
+
+    it "belongs to a user" do
+      assc = described_class.reflect_on_association(:user)
+      expect(assc.macro).to eq :belongs_to
+    end
+  end
 end
