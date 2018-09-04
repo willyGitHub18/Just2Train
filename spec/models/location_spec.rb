cd8a662f06b002c771e1084c3258fa0e2d252c68
willy <wilg1569@gmail.com>
2018-09-04 17:50:47 +0200

    Test de validation pour location

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Use of subject method so the tests will apply to the subject
+  subject{ described_class.new(street1: "some_street", city: "some_city", zip: "some_zipcode") }
+
+  describe "Validation" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without a street" do
+      subject.street1 = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a city" do
+      subject.city = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a zipcode" do
+      subject.zip = nil
+      expect(subject).to_not be_valid
+    end
+  end
 end
