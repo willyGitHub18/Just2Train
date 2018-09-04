4948fac9d7a4e814dd699376b1a26c495da1596f
willy <wilg1569@gmail.com>
2018-09-04 14:43:58 +0200

    Jointure tables pour event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :location
   belongs_to :activity
+  has_many :event_participants, dependent: :destroy
   has_many :users, through: :event_participants
 end
