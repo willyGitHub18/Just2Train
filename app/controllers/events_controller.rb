99e0863b5a3d2e5488a8755aa42b93123c2ac994
willy <wilg1569@gmail.com>
2018-09-05 14:04:21 +0200

    Préparation controller Event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,11 +12,18 @@ class EventsController < ApplicationController
   end
 
   def create
+    chosen_activity = Activity.find(params[:activity_id])
+    chosen_location = Location.find(params[:location_id])
 
+    @event = Event.new
+    @event.activity = chosen_activity
+    @event.location = chosen_location
+    @event.save
+    redirect_to event_path
   end
 
   private
     def event_params
-
+      params.require(:event).permit(:name, :price, :date, :nb_participant, :level, :time, :activity_id, :location_id)
     end
 end
