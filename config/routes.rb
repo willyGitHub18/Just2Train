a5809e8a023a235f3b72d62f78b6d34a80df1edb
willy <wilg1569@gmail.com>
2018-09-05 12:25:26 +0200

    Tests sur controller Event

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#index'
 
-  get 'events/index'
-  get 'events/show'
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#index'
+
+  resources :events
+
 end
