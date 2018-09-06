9c3c6b341927747929de035360841abcb4f4d3af
Carlos CANA <carloscana@gmail.com>
2018-09-06 16:34:40 +0200

    Modif schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,73 +10,73 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_04_132636) do
+ActiveRecord::Schema.define(version: 2018_09_06_095037) do
 
-  create_table "activities", force: :cascade do |t|
-    t.string "type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ create_table "activities", force: :cascade do |t|
+   t.string "activity_name"
+   t.datetime "created_at", null: false
+   t.datetime "updated_at", null: false
+ end
 
-  create_table "event_participants", force: :cascade do |t|
-    t.boolean "is_creator", default: true
-    t.boolean "is_admin", default: true
-    t.integer "event_id"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ create_table "event_participants", force: :cascade do |t|
+   t.boolean "is_creator", default: true
+   t.boolean "is_admin", default: true
+   t.integer "event_id"
+   t.integer "user_id"
+   t.datetime "created_at", null: false
+   t.datetime "updated_at", null: false
+ end
 
-  create_table "events", force: :cascade do |t|
-    t.string "name"
-    t.decimal "price", default: "0.0"
-    t.date "date"
-    t.integer "nb_participant"
-    t.integer "level"
-    t.time "time"
-    t.integer "activity_id"
-    t.integer "location_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ create_table "events", force: :cascade do |t|
+   t.string "name"
+   t.decimal "price", default: "0.0"
+   t.date "date"
+   t.integer "nb_participant"
+   t.integer "level"
+   t.time "time"
+   t.integer "activity_id"
+   t.integer "location_id"
+   t.datetime "created_at", null: false
+   t.datetime "updated_at", null: false
+ end
 
-  create_table "locations", force: :cascade do |t|
-    t.string "category"
-    t.string "place_name"
-    t.string "street1"
-    t.string "street2"
-    t.string "city"
-    t.string "zip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ create_table "locations", force: :cascade do |t|
+   t.string "category"
+   t.string "place_name"
+   t.string "street1"
+   t.string "street2"
+   t.string "city"
+   t.string "zip"
+   t.datetime "created_at", null: false
+   t.datetime "updated_at", null: false
+ end
 
-  create_table "user_activities", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "activity_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ create_table "user_activities", force: :cascade do |t|
+   t.integer "user_id"
+   t.integer "activity_id"
+   t.datetime "created_at", null: false
+   t.datetime "updated_at", null: false
+ end
 
-  create_table "users", force: :cascade do |t|
-    t.string "first_name", default: "", null: false
-    t.string "last_name", default: "", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "phone", default: "", null: false
-    t.string "street1", default: "", null: false
-    t.string "street2", default: "", null: false
-    t.string "city", default: "", null: false
-    t.string "zip", default: "", null: false
-    t.string "profile_picture", default: "", null: false
-    t.text "description", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
+ create_table "users", force: :cascade do |t|
+   t.string "email", default: "", null: false
+   t.string "encrypted_password", default: "", null: false
+   t.string "reset_password_token"
+   t.datetime "reset_password_sent_at"
+   t.datetime "remember_created_at"
+   t.datetime "created_at", null: false
+   t.datetime "updated_at", null: false
+   t.string "username"
+   t.string "full_name"
+   t.text "biography"
+   t.string "profile_picture"
+   t.string "phone"
+   t.string "street1"
+   t.string "street2"
+   t.string "city"
+   t.string "zip"
+   t.index ["email"], name: "index_users_on_email", unique: true
+   t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+ end
 
 end
