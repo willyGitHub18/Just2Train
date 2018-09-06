8ef4d14c293d533e8d16788a630d9c68ecd8356b
Mario <mario.antoniali@gmail.com>
2018-09-06 13:55:30 +0200

    remove sign in obligation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :username, length: { in: 3..20 }
-  validates :phone, presence: true, format: { with: /\A(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}\z/, message: "phone format not recognized" }
+  validates :phone, presence: true, format: { with: /\A(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}\z/, message: "phone number format not recognized" }
   validates :biography, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed" }
  
   has_many :user_activities, dependent: :destroy
