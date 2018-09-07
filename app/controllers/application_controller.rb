4b3ac6d2a00cc6155525c55e86a5523a4d9bd27d
willy <wilg1569@gmail.com>
2018-09-07 14:34:13 +0200

    Correction indentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,9 @@
 class ApplicationController < ActionController::Base
-        
-    
-        before_action :configure_permitted_parameters, if: :devise_controller?
-        protected
-            def configure_permitted_parameters
-            devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:password, :password_confirmation, :current_password, :email, :username, :full_name, :biography, :phone, :street1, :street2, :city, :zip, :profile_picture) }
-            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:password, :password_confirmation, :current_password, :email, :username, :full_name, :biography, :phone, :street1, :street2, :city, :zip, :profile_picture) }
-            end
-end
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:password, :password_confirmation, :current_password, :email, :username, :full_name, :biography, :phone, :street1, :street2, :city, :zip, :profile_picture) }
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:password, :password_confirmation, :current_password, :email, :username, :full_name, :biography, :phone, :street1, :street2, :city, :zip, :profile_picture) }
+    end
+end
