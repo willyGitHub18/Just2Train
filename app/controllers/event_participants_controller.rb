018911822da1c2f91fed72eac17ee2ba99d166a5
willy <wilg1569@gmail.com>
2018-09-07 15:46:55 +0200

    Ajout participants à évenement

--- app/controllers/event_participants_controller.rb
+++ app/controllers/event_participants_controller.rb
@@ -1,6 +1,18 @@
 class EventParticipantsController < ApplicationController
   def create
+    chosen_event = Event.find(params[:event_id])
 
+    @event_participant = EventParticipant.new
+    @event_participant.user = current_user
+    @event_participant.event = chosen_event
+
+    if @event_participant.save
+      flash[:notice] = "Thank you your are now registered. We are counting on your presence ;)"
+      redirect_to root_path
+    else
+      flash.now[:warning] = "The seem to be problems when submitting your registration, please try again"
+      render event_path
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
 
-  devise_for :users, :controllers => { :registrations => "devise/registrations" }
-
   get '/about', to: 'static_pages#about'
+
+  get 'event/:id', to: 'events#show', as: "event"
+  post 'event_participants', to: 'event_participants#create', as: "event_participants"
+
+  devise_for :users, :controllers => { :registrations => "devise/registrations" }
 
   resources :events
   resources :locations
