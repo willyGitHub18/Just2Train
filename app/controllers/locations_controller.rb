2eb9f2d4f139ae6f0735389502fef8a533ba407d
willy <wilg1569@gmail.com>
2018-09-10 22:10:03 +0200

    Edit event

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -10,11 +10,25 @@ class LocationsController < ApplicationController
     @location = Location.new(location_params)
     @location.save
     flash[:success] = "Congrats, your event has been created"
-    redirect_to 'root_url'
+    redirect_to @location
+  end
+
+  def edit
+    @location = Location.find(params[:id])
+  end
+
+  def update
+    @location = Location.find(params[:id])
+    if @location.update_attributes(location_params)
+      flash|:success] = "Event updated"
+      redirect_to @location
+    else
+      render 'edit'
+    end
   end
 
   private
     def location_params
-      params.require(:location).permit(:category, :place_name, :street1, :street2, :city, :zip, events_attributes: [ :name, :price, :date, :nb_participant, :level, :time, :activity_id, :location_id ] )
+      params.require(:location).permit(:category, :place_name, :street1, :street2, :city, :zip, events_attributes: [ :id, :name, :price, :date, :nb_participant, :level, :time, :activity_id, :location_id ] )
     end
 end
