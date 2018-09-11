1128c44c6a02477cb373e07063db0427f63e7b77
Carlos CANA <carloscana@gmail.com>
2018-09-11 11:48:58 +0200

    Change before_filter(deprecated) to before_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
-  before_filter :set_user, only: [:show]
+  before_action :set_user, only: [:show]
   def show
+
   end
+
   private
 
   def set_user
     @user = User.find(params[:id])
   end
+
+end
