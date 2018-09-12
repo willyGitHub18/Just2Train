2cd7885c6debd1787ca521e6e2179c1a5d1ecb92
willy <wilg1569@gmail.com>
2018-09-12 10:16:32 +0200

    Correction indentation code

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
     before_action do
      @conversation = Conversation.find(params[:conversation_id])
     end
+    
   def index
    @messages = @conversation.messages
     if @messages.length > 10
@@ -19,15 +20,18 @@ class MessagesController < ApplicationController
    end
   @message = @conversation.messages.new
    end
+
   def new
    @message = @conversation.messages.new
   end
+
   def create
    @message = @conversation.messages.new(message_params)
    if @message.save
     redirect_to conversation_messages_path(@conversation)
    end
   end
+
   private
    def message_params
     params.require(:message).permit(:body, :user_id)
