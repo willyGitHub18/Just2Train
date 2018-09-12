1fffc44bcfe5d99f04c1f0592cbdcd9956f4e469
willy <wilg1569@gmail.com>
2018-09-12 10:23:25 +0200

    Correction indentation code

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,7 @@
 class MessagesController < ApplicationController
-    before_action do
-      @conversation = Conversation.find(params[:conversation_id])
-    end
+  before_action do
+    @conversation = Conversation.find(params[:conversation_id])
+  end
 
   def index
     @messages = @conversation.messages
