3220d0b06605bb065249cc6329cbebad616821a8
willy <wilg1569@gmail.com>
2018-09-13 11:03:10 +0200

    Style images landing page

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,37 +1,38 @@
 class LocationsController < ApplicationController
-  before_action :authenticate_user!
-  def new
-    @location = Location.new
-    # Needed to seed the form with inputs of events
-    @location.events.new
-  end
+    before_action :authenticate_user!
 
-  def create
-    @location = Location.new(location_params)
-    @location.save
-    flash[:success] = "Congrats, your event has been created"
-    redirect_to events_path
-  end
-
-  def edit
-    @location = Location.find(params[:id])
-  end
-
-  def update
-    @location = Location.find(params[:id])
-    if @location.update_attributes(location_params)
-      flash[:success] = "Event updated"
-      redirect_to events_path
-    else
-      render 'edit'
+    def new
+      @location = Location.new
+      # Needed to seed the form with inputs of events
+      @location.events.new
     end
-  end
 
-  def destroy
-    Location.find(params[:id]).destroy
-    flash[:success] = "Event deleted"
-    redirect_to root_path
-  end
+    def create
+      @location = Location.new(location_params)
+      @location.save
+      flash[:success] = "Congrats, your event has been created"
+      redirect_to events_path
+    end
+
+    def edit
+      @location = Location.find(params[:id])
+    end
+
+    def update
+      @location = Location.find(params[:id])
+      if @location.update_attributes(location_params)
+        flash[:success] = "Event updated"
+        redirect_to events_path
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+      Location.find(params[:id]).destroy
+      flash[:success] = "Event deleted"
+      redirect_to root_path
+    end
 
   private
     def location_params
