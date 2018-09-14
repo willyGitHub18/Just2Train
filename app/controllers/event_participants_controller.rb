24ad66f8c9d5f60851c86c0e6af7de64d204d12e
willy <wilg1569@gmail.com>
2018-09-14 10:27:49 +0200

    Blocage registration quand event booked up

--- app/controllers/event_participants_controller.rb
+++ app/controllers/event_participants_controller.rb
@@ -3,18 +3,26 @@ class EventParticipantsController < ApplicationController
   def create
     chosen_event = Event.find(params[:event_id])
 
-    @event_participant = EventParticipant.new
-    @event_participant.user = current_user
-    @event_participant.event = chosen_event
-    @event_participant.is_creator = false
-    @event_participant.is_admin = false
+    # Counting the number of participants already registered to a particular event
+    registered = EventParticipant.where(event_id: chosen_event.id).count
 
-    if @event_participant.save
-      flash[:notice] = "Thank you your are now registered. We are counting on your presence ;)"
-      redirect_to events_path
+    if chosen_event.nb_participant == registered
+      flash[:notice] = "Sorry the event is booked up but you can still organize your own event"
+      redirect_to new_location_path
     else
-      flash.now[:warning] = "A problem occured while you submit your registration, please try again"
-      render event_path
+      @event_participant = EventParticipant.new
+      @event_participant.user = current_user
+      @event_participant.event = chosen_event
+      @event_participant.is_creator = false
+      @event_participant.is_admin = false
+
+      if @event_participant.save
+        flash[:notice] = "Thank you your are now registered. We are counting on your presence ;)"
+        redirect_to events_path
+      else
+        flash.now[:warning] = "A problem occured while you submit your registration, please try again"
+        render event_path
+      end
     end
   end
 
