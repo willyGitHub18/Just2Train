22321a5724f6d06588f78aa4732f8852a583500f
willy <wilg1569@gmail.com>
2018-09-14 18:02:11 +0200

    Filtre en fonction activité

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,14 @@ Rails.application.routes.draw do
 
   get '/about', to: 'static_pages#about'
 
+  get 'events/futsal', to: 'events#index_futsal', as: "futsal"
+  get 'events/cyclism', to: 'events#index_cyclism', as: "cyclism"
+  get 'events/running', to: 'events#index_running', as: "running"
+  get 'events/basketball', to: 'events#index_basketball', as: "basketball"
+  get 'events/football', to: 'events#index_football', as: "football"
+
   get 'event/:id', to: 'events#show', as: "event"
+  
   post 'event_participants', to: 'event_participants#create', as: "event_participants"
 
   devise_for :users, :controllers => { :registrations => 'users/registrations' }
