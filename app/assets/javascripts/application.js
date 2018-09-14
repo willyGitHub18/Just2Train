cbb07f6c8f85af298ad5afdf93b294057fb0563c
willy <wilg1569@gmail.com>
2018-09-14 15:29:08 +0200

    Dropdown Heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,14 +10,14 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
-//= require_tree .
 // Bootstrap dependencies
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
 
 $(document).ready(function(){
   $('.dropdown-toggle').dropdown();
